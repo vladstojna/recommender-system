57c0cd0b9cbcdc403a70cbe456a92fd2bc86408b
vladstojna <vlad.leic@gmail.com>
2020-03-27 13:50:21 +0000

    add toggle for benchmarks

--- benchmark.h
+++ benchmark.h
@@ -1,5 +1,9 @@
 #include <time.h>
 
+#define ENABLE_BENCH
+
+#ifdef ENABLE_BENCH
+
 #define __init_benchmark \
 	struct timespec __start_timespec__; \
 	struct timespec __finish_timespec__;
@@ -18,10 +22,17 @@
 	} \
 	printf("%s : %f\n", msg, __finish_timespec__.tv_sec * (double) prec + __finish_timespec__.tv_nsec / ((1e9) / (double) prec));
 
+#else
+
+#define __init_benchmark // empty
+#define __start_benchmark // empty
+#define __end_benchmark(msg, prec) // empty
+
+#endif
+
 /* Usage
 __init_benchmark // only once
 __start_benchmark
 work();
 __end_benchmark("benchmark 1", 1e3)
 */
-
