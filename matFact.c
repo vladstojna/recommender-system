6583599ce737fd1efdc8133267c75d9c16cc2470
vladstojna <vlad.leic@gmail.com>
2020-03-27 13:50:32 +0000

    add output printing

--- matFact.c
+++ matFact.c
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define swap(T, a, b) { T tmp = a; a = b; b = tmp; }
+
 typedef struct
 {
 	int row;
@@ -21,6 +23,9 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Run ./matFact.out file");
 		die("Missing input file name.");
 	}
+
+	__init_benchmark;
+	__start_benchmark
 
 	const char *file_name = argv[1];
 	FILE *fp = fopen(file_name, "r");
@@ -79,7 +84,8 @@ int main(int argc, char **argv)
 
 	mat2d *B = mat2d_new(users, items);
 
-	__init_benchmark;
+	__end_benchmark("input", 1)
+
 	__start_benchmark;
 
 	for (int iter = 0; iter < iters; iter++)
@@ -110,22 +116,31 @@ int main(int argc, char **argv)
 			for (int c = 0; c < features; c++)
 				mat2d_set(R, r, c, mat2d_get(R_stable, r, c) - alpha * 2 * mat2d_get(R, r, c));
 
-		mat2d *tmp = L_stable;
-		L_stable = L;
-		L = tmp;
-
-		tmp = R_stable;
-		R_stable = R;
-		R = tmp;
+		swap(mat2d*, L, L_stable);
+		swap(mat2d*, R, R_stable);
 	}
-
-	__end_benchmark("main loop", 1e3);
-
 	mat2d_prod(L_stable, R_stable, B);
 
-	// mat2d_print(L_stable);
-	// mat2d_print(R_stable);
-	// mat2d_print(B);
+	__end_benchmark("main loop", 1);
+
+	// print output
+	__start_benchmark
+
+	for (int i = 0, aix = 0; i < users; i++) {
+		int max = 0;
+		for (int j = 0; j < items; j++) {
+			if (!(entries[aix].row == i && entries[aix].col == j)) {
+				if (mat2d_get(B, i, j) > mat2d_get(B, i, max)) {
+					max = j;
+				}
+			} else {
+				aix++;
+			}
+		}
+		printf("%d\n", max);
+	}
+
+	__end_benchmark("output", 1)
 
 	mat2d_free(L_stable);
 	mat2d_free(R_stable);
