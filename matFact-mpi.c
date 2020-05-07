f77e58a5638ffb230806d5e6f1491cbf77eefcd0
TricksterByte <psilvacenteno@gmail.com>
2020-05-07 17:35:07 +0100

    checkboard decomposition init for matrix L

--- matFact-mpi.c
+++ matFact-mpi.c
@@ -127,6 +127,37 @@ void print_output(mat2d *B, non_zero_entry *entries) {
 	}
 }
 
+// void matrix_factorization_v2() {
+
+// 	for (int iter = 0; iter < iters; iter++) {
+// 		for (int n = 0; n < non_zero; n++) {
+// 			entry = entries[n];
+// 			row = entry.i;
+// 			col = entry.j;
+
+// 			if row in L.block {
+// 				if col in R.block {
+// 					calculate partial dot;
+// 					reduce;
+// 					update L and R lines;
+// 				} else {
+// 					send block;
+// 					recv other block;
+// 					calculate partial dot;
+// 					reduce;
+// 					update L;
+// 				}
+// 			} else if col in R.block {
+// 				send block;
+// 				recv other block;
+// 				calculate partial dot;
+// 				reduce;
+// 				update R;
+// 			}
+// 		}
+// 	}
+// }
+
 void matrix_factorization(int id, int p, double *L, double *R, input_info *info) {
 	int iters = info->dataset_info.iters;
 	int features = info->dataset_info.features;
@@ -188,6 +219,71 @@ void matrix_factorization(int id, int p, double *L, double *R, input_info *info)
 	free(R_zero);
 }
 
+double *initialize_L(MPI_Comm cart_comm, dataset_info info) {
+	int grid_id, nproc;
+
+	MPI_Comm_rank(cart_comm, &grid_id);
+	MPI_Comm_size(cart_comm, &nproc);
+
+	int rows = info.users;
+	int cols = info.features;
+
+	int grid_size[2], grid_periodic[2], grid_coord[2];
+	MPI_Cart_get(cart_comm, 2, grid_size, grid_periodic, grid_coord);
+
+	int nlocal_rows = BLOCK_SIZE(grid_coord[0], grid_size[0], rows);
+	int nlocal_cols = BLOCK_SIZE(grid_coord[1], grid_size[1], cols);
+
+	double *L_local = malloc(sizeof(double) * nlocal_rows * nlocal_cols);
+
+	double *buffer;
+	if (is_root(grid_id)) {
+		buffer = malloc(sizeof(double) * cols);
+	}
+
+	int block_coord[2];
+	int dest_id;
+	double *source_address, *dest_address;
+	for (int i = 0; i < grid_size[0]; i++) {
+		block_coord[0] = i;
+		for (int j = 0; j < BLOCK_SIZE(i, grid_size[0], rows); j++) {
+			if (is_root(grid_id)) {
+				for (int f = 0; f < cols; f++) {
+					buffer[f] = RAND01 / cols;
+				}
+			}
+
+			for (int k = 0; k < grid_size[1]; k++) {
+				block_coord[1] = k;
+				MPI_Cart_rank(cart_comm, block_coord, &dest_id);
+
+				if (is_root(grid_id)) {
+					source_address = buffer + BLOCK_LOW(k, grid_size[1], cols);
+
+					if (is_root(dest_id)) {
+						dest_address = &L_local[j * nlocal_cols];
+						memcpy(dest_address, source_address, nlocal_cols * sizeof(double));
+					} else {
+						int blocksize = BLOCK_SIZE(k, grid_size[1], cols);
+
+						MPI_Send(source_address, blocksize, MPI_DOUBLE, dest_id, 0, cart_comm);
+					}
+				}
+
+				else if (grid_id == dest_id) {
+					MPI_Recv(&L_local[j * nlocal_cols], nlocal_cols, MPI_DOUBLE, 0, 0, cart_comm, 0);
+				}
+			}
+		}
+	}
+
+	if (is_root(grid_id)) {
+		free(buffer);
+	}
+
+	return L_local;
+}
+
 int main(int argc, char **argv)
 {
 	if (argc != 2)
@@ -202,14 +298,24 @@ int main(int argc, char **argv)
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 
-	printf("Init rank = %d\n", id);
+	int dims[2] = { 0 };
+	int reorder = 1;
+	int wraparound[2] = { 1, 1 };
+	MPI_Comm grid_comm;
+
+	MPI_Dims_create(nproc, 2, dims);
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, wraparound, reorder, &grid_comm);
+
+	int coords[2] = { 0 };
+	MPI_Cart_coords(grid_comm, id, 2, coords);
+	int cart_rank;
+	MPI_Cart_rank(grid_comm, coords, &cart_rank);
 
 	input_info local;
 	if (is_root(id)) {
 		if (read_input(argv[1], &local) != 0) {
 			die("Unable to initialize parameters.");
 		}
-		print_dataset_info(&local.dataset_info);
 	}
 
 	// Creating MPI types
@@ -220,18 +326,21 @@ int main(int argc, char **argv)
 
 	MPI_Bcast(&local.dataset_info, 1, dataset_info_type, 0, MPI_COMM_WORLD);
 
-	printf("Received dataset info, rank = %d\n", id);
-
 	if (!is_root(id)) {
 		local.entries = malloc(sizeof(non_zero_entry) * local.dataset_info.non_zero_sz);
 	}
 
 	MPI_Bcast(local.entries, local.dataset_info.non_zero_sz, non_zero_type, 0, MPI_COMM_WORLD);
 
-	printf("Received non-zero entries, rank = %d\n", id);
+	double *L_local = initialize_L(grid_comm, local.dataset_info);
+	for (int i = 0; i < BLOCK_SIZE(coords[0], dims[0], local.dataset_info.users) * BLOCK_SIZE(coords[1], dims[1], local.dataset_info.features); i++) {
+		printf("%f\n", L_local[i]);
+		fflush(stdout);
+	}
 
 	// Creating L and R
 	mat2d *L = mat2d_new(local.dataset_info.users, local.dataset_info.features);
+
 	// R is always assumed transposed
 	mat2d *R = mat2d_new(local.dataset_info.items, local.dataset_info.features);
 
@@ -239,16 +348,15 @@ int main(int argc, char **argv)
 	if (is_root(id)) {
 		mat2d *R_init = mat2d_new(local.dataset_info.features, local.dataset_info.items);
 		mat2d_random_fill_LR(L, R_init, local.dataset_info.features);
+		mat2d_print(R_init);
 		mat2d_transpose(R_init, R);
+		mat2d_print(R);
 		mat2d_free(R_init);
 	}
-
-	printf("Initialized L and R, rank = %d\n", id);
 
 	// MPI_Barrier(MPI_COMM_WORLD);
 	matrix_factorization(id, nproc, L->data, R->data, &local);
 
-	// print output
 	if (is_root(id)) {
 		mat2d *B = mat2d_new(local.dataset_info.users, local.dataset_info.items);
 		mat2d_prod(L, R, B);
